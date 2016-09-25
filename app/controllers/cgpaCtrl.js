576c06d9e1f5d55642529b337e0e152c8362c485
tenorz007 <chizzy.alaedu@aun.edu.ng>
2016-09-25 18:36:01 +0100

    Added gpa calculation functionality
    * Added view and methods to handle gpa calculation

--- app/controllers/cgpaCtrl.js
+++ app/controllers/cgpaCtrl.js
@@ -4,13 +4,21 @@ angular
     .module('cgpaCalculator')
     .controller('cgpaCtrl', function($scope, cgpaFactory) {
 
-        $scope.grades = cgpaFactory.getGrades()
-        $scope.gradePoints = cgpaFactory.getGradePoints()
+        $scope.grades = cgpaFactory.getGrades();
+        $scope.gradePoints = cgpaFactory.getGradePoints();
+        $scope.creditHours = cgpaFactory.getCreditHours();
 
+        $scope.results = {};
+        $scope.answer = {};
         $scope.listSize = 5;
 
         $scope.getListSize = function(number) {
             return new Array(number);
         }
+
+        $scope.getCalculatedGrades = function() {
+            $scope.answer = cgpaFactory.calculateGrades($scope.results);
+        }
+
 
     });
--- app/services/cgpaFactory.js
+++ app/services/cgpaFactory.js
@@ -17,9 +17,42 @@ angular
             return Object.keys(grades);
         }
 
+        function getCreditHours() {
+            return [4, 3, 2, 1, 0];
+        }
+
+        function calculateGPA(data) {
+            var gradePoints = getGradePoints();
+            var result = {'credits' : 0, 'gpa' : 0, 'total' : 0};
+
+            for (var dataKey in data) {
+                if (!data.hasOwnProperty(dataKey)) {
+                    continue;
+                }
+
+                if ('hour', 'grade' in data[dataKey]) {
+                    if (!['W', 'WP'].includes(data[dataKey].grade)) {
+                        var credit = gradePoints[data[dataKey].grade] * data[dataKey].hour;
+                        result['credits'] += data[dataKey].hour;
+                        result['total'] += credit;
+                    }
+                }
+            }
+
+            result['gpa'] = result['total']/result['credits'];
+            return result;
+        }
+
+        function calculateGrades(data) {
+            var result = calculateGPA(data)
+            return result;
+        }
+
         return {
             getGradePoints : getGradePoints,
-            getGrades : getGrades
+            getGrades : getGrades,
+            getCreditHours : getCreditHours,
+            calculateGrades : calculateGrades
         }
 
     });
