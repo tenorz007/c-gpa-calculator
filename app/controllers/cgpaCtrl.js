11c9d6f064149229d85e60c25f3d80e64c56f0ba
tenorz007 <chizzy.alaedu@aun.edu.ng>
2016-09-26 21:49:54 +0100

    Updated naming convention

--- app/controllers/cgpaCtrl.js
+++ app/controllers/cgpaCtrl.js
@@ -8,11 +8,16 @@ angular
         $scope.gradePoints = cgpaFactory.getGradePoints();
         $scope.creditHours = cgpaFactory.getCreditHours();
 
-        $scope.results = {"rows" : new Array(5)};
+        $scope.data = {"rows" : new Array(5)};
         $scope.answer = {};
 
         $scope.getCalculatedGrades = function() {
-            $scope.answer = cgpaFactory.calculateGrades($scope.results);
+            $scope.data["cgpaExpected"] = 3.83;
+            $scope.data["currentCredits"] = 16;
+            $scope.data["cgpa"] = 3.56;
+            $scope.data["creditsCompleted"] = 15;
+
+            $scope.result = cgpaFactory.calculateGrades($scope.data);
         }
 
 
