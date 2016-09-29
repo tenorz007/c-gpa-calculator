83fb1d4a4f12a9a605ac6c329b7ed461bbbdf895
tenorz007 <chizzy.alaedu@aun.edu.ng>
2016-09-29 21:27:57 +0100

    Added watch to form to make calculations reactive

--- app/controllers/cgpaCtrl.js
+++ app/controllers/cgpaCtrl.js
@@ -9,16 +9,19 @@ angular
         $scope.creditHours = cgpaFactory.getCreditHours();
 
         $scope.data = {"rows" : new Array(5)};
-        $scope.answer = {};
 
-        $scope.getCalculatedGrades = function() {
+        $scope.reset = function() {
+            $scope.data.rows = new Array(5);
+        };
+
+        $scope.$watch("data", function() {
             $scope.data["cgpaExpected"] = 3.83;
             $scope.data["currentCredits"] = 16;
             $scope.data["cgpa"] = 3.56;
             $scope.data["creditsCompleted"] = 15;
-
+            console.log("form changed");
             $scope.result = cgpaFactory.calculateGrades($scope.data);
-        }
+        }, true)
 
 
     });
