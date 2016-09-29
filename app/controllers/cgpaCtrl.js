48d09992af841f0cf550d3c7c22b7986123ceb60
tenorz007 <chizzy.alaedu@aun.edu.ng>
2016-09-29 22:49:40 +0100

    Added fields for other inputs

--- app/controllers/cgpaCtrl.js
+++ app/controllers/cgpaCtrl.js
@@ -15,10 +15,6 @@ angular
         };
 
         $scope.$watch("data", function() {
-            $scope.data["cgpaExpected"] = 3.83;
-            $scope.data["currentCredits"] = 16;
-            $scope.data["cgpa"] = 3.56;
-            $scope.data["creditsCompleted"] = 15;
             console.log("form changed");
             $scope.result = cgpaFactory.calculateGrades($scope.data);
         }, true)
